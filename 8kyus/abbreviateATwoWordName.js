223cb7b859b586a6ad829e425255f73668c3eb44
efs0-cod3 <eflearnng@gmail.com>
2022-05-12 05:37:36 -0400

    Second KOD

---
+++ 8kyus/abbreviateATwoWordName.js
@@ -0,0 +1,12 @@
+function abbrevName(name){
+    // code away
+    name = name.split('')
+   let firstInitial =name[0] + '.';
+   let secondInitial = '';
+   for(let l = 0; l < name.length; l++){
+     if(name[l]== ' '){
+   secondInitial = name[l+1]
+     }
+   }
+   return (firstInitial+secondInitial).toUpperCase()
+   }
