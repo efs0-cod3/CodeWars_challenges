6a7bca7f3bcff2edeb347fd80f6baa3344acc500
efs0-cod3 <eflearnng@gmail.com>
2022-05-13 06:49:01 -0400

    breakCamelCase

---
+++ 6kyus/breakCamelCase.js
@@ -0,0 +1,9 @@
+function solution(string) {
+    return string.split('').map(letter => {
+     if (letter === letter.toUpperCase()) {
+    return  " " + letter
+     } else
+      return letter
+    }).join('')
+    }
+    
