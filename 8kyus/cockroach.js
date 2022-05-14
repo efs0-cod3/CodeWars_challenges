a474aed692072705b5d225d97855451d22c5c6aa
efs0-cod3 <eflearnng@gmail.com>
2022-05-14 07:12:20 -0400

    kod

---
+++ 8kyus/cockroach.js
@@ -0,0 +1,11 @@
+//  p they give me sec
+//  r return cm/s
+//  e 1.08 -> 30
+//  p get formula for an approximate result, 
+//    multiply the speed value by 27.778.
+//    and round down the number.
+
+function cockroachSpeed(s) {
+    //Good Luck!
+    return Math.floor(s * 27.778)  
+  }
