b73117a900d36968ac41b2940232d88a6260b2a6
efs0-cod3 <eflearnng@gmail.com>
2022-06-08 05:43:13 -0400

    mod

--- 7kyus/binaryAddition.js
+++ 7kyus/binaryAddition.js
@@ -3,3 +3,15 @@ function addBinary(a, b) {
     console.log(sum.toString(2))
     return sum.toString(2)
 }
+
+function addBinary(a, b) {
+    var sum = a + b,
+        binary = '';
+
+    while (sum > 0) {
+        binary = (sum % 2) + binary;
+        sum = Math.floor(sum / 2);
+    }
+
+    return binary;
+}
--- binaryAddition.js
+++ binaryAddition.js
@@ -1,5 +1,18 @@
-function addBinary(a,b) {
-    let sum = a+b
+function addBinary(a, b) {
+    let sum = a + b
     console.log(sum.toString(2))
     return sum.toString(2)
-   }
+}
+
+//  other solution
+function addBinary(a, b) {
+    var sum = a + b,
+        binary = '';
+
+    while (sum > 0) {
+        binary = (sum % 2) + binary;
+        sum = Math.floor(sum / 2);
+    }
+
+    return binary;
+}
