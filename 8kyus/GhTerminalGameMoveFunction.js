c208c3211fb64e61f23bb1e3f6ecef018dcde57e
efs0-cod3 <eflearnng@gmail.com>
2022-06-12 05:15:57 -0400

    kod 2+

---
+++ 8kyus/GhTerminalGameMoveFunction.js
@@ -0,0 +1,5 @@
+
+function move (position, roll) {
+    // return the new position
+    return position + (roll * 2);
+  }
---
+++ drinkAbout.js
@@ -0,0 +1,3 @@
+function peopleWithAgeDrink(old) {
+    return old >= 21 ? "drink whisky" : old >= 18 ? "drink beer" : old >= 14? "drink coke": "drink toddy"
+   };
