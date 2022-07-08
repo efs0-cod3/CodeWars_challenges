4da2e01c99205aafca7f221055cb359e9a54bd4b
efe <efe@efes-MBP.home>
2022-07-08 07:39:20 -0400

    im back

---
+++ 7kyus/isAnagram.js
@@ -0,0 +1,21 @@
+// 100
+
+// write the function isAnagram
+
+// P => got two strings to check if they are anagram for each other
+// R => boolean 
+// E => "foefet" == "toffee" => true
+// P => make it case insensitive,
+//      split str into arr, 
+//      sort them and join them into a str.
+//      compare the strings.
+
+var isAnagram = function(test, original) {
+    test = test.toLowerCase().split('').sort().join('')
+    original = original.toLowerCase().split('').sort().join('')
+      
+    return test == original
+
+    };
+
+// isAnagram('test','tste')
