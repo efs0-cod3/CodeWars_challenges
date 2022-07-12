0f81386b4ce5c791e878acec1aaf63798eda3db1
efs0-cod3 <eflearnng@gmail.com>
2022-07-12 09:02:09 -0400

    kod check exam

---
+++ 8kyus/checkExam.js
@@ -0,0 +1,17 @@
+function checkExam(array1, array2) {
+    // good luck
+let result = 0
+for(let i =0; i < array1.length;i++){
+    if(array1[i] == array2[i]){
+    result += 4
+    } else if(array1[i] != array2[i] && array2[i] == ''){
+    result += 0
+    }else {
+    result -= 1
+    }
+}
+
+if (result < 0)result = 0
+return result
+
+}
