2aec9d64cd6e345476962054ab1ca26dc1fc221e
efs0-cod3 <eflearnng@gmail.com>
2022-07-14 06:55:37 -0400

    kod

--- maximunArrSequence.js
+++ maximunArrSequence.js
@@ -21,3 +21,22 @@ var maxSequence = function(arr){
     }
     return maxNow;
 }
+
+//other solution
+var maxSequence = function(arr){
+  // Parameters: arr: an array of numbers
+  // Return: the maximum sum of contiguous numbers
+  // Example: maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6
+  // Pseudocode: set a counter to 0, loop through the array, starting with each indice greater than 0,
+  // then add the next indice to it. Compare to counter (and to self), then either stop or continue
+  let result = 0
+  for (let i = 0; i < arr.length; i++) {
+    let tracker = 0
+    for (let j = i + 1; j <= arr.length; j++) {
+      let cur = arr.slice(i, j).reduce((a,c) => a + c, 0)
+      if (cur > tracker) tracker = cur
+    }
+    if (tracker > result) result = tracker
+  }
+  return result
+}
