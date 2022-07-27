b8549c425f53800b96254455345dbaab0888e74c
efs0-cod3 <eflearnng@gmail.com>
2022-07-27 11:58:49 -0400

    kod abc

---
+++ findThePosition.js
@@ -0,0 +1,35 @@
+function position(letter) {
+  //Write your own Code!
+  let abc = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let index = abc.indexOf(letter) + 1;
+  return `Position of alphabet: ${index}`;
+}
+
+console.log(position('t'));
