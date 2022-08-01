bbd3c3501be630570dce8ee46299793ebdae061b
efs0-cod3 <eflearnng@gmail.com>
2022-08-01 07:35:16 -0400

    kod

--- 7kyus/sumOfMinimuns.js
+++ 7kyus/sumOfMinimuns.js
@@ -6,7 +6,7 @@ function sumOfMinimums(arr) {
     }
     return result;
   }
-
+  sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]])
 // with reduce
 
 //   function sumOfMinimums(arr) {
