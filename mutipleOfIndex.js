6d00388792e198d681f781e706291a01b690d094
efs0-cod3 <eflearnng@gmail.com>
2022-08-02 06:34:20 -0400

    kod moi

---
+++ mutipleOfIndex.js
@@ -0,0 +1,11 @@
+function multipleOfIndex(array) {
+    // good luck
+    let output = []
+    array.forEach((e,i) => {
+      if(e % i == 0){
+        output.push(e)
+      }
+    })
+    console.log(output)
+    return output
+  }
