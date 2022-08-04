903fb0554ea9f6fd6f61cc0f1cd168b161c13c7c
efs0-cod3 <eflearnng@gmail.com>
2022-08-04 14:14:01 -0400

    kod

---
+++ productOfConsecutiveFibNum.js
@@ -0,0 +1,16 @@
+function productFib(prod){
+    // ...
+  let n1 = 0, n2 = 1, nextNum
+  nextNum = n1 + n2
+    while(nextNum != prod){
+      nextNum = n1 + n2
+      n1=n2
+      n2=nextNum
+  
+    if(n1*n2 == prod){
+      return [n1,n2,n1*n2 == prod]
+    }else if(n1*n2 > prod){
+      return [n1,n2,n1*n2 == prod]
+    }
+    }  
+  }
