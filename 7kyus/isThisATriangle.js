ecd6c81a361703f9e0f63edd11af6428b4ac7adb
efs0-cod3 <eflearnng@gmail.com>
2022-09-03 19:55:12 -0400

    kod

---
+++ 7kyus/isThisATriangle.js
@@ -0,0 +1,5 @@
+function isTriangle(a,b,c)
+{
+return (a+b > c && b+c > a && a+c > b) ? true : false
+
+}
---
+++ 8kyus/enumerableMagic#25.js
@@ -0,0 +1,4 @@
+function take(arr, n) {
+    // Your code here
+    return arr.slice(0,n)
+  }
