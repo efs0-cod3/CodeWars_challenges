fb47883a9eef77320d17168780f19ac15a5c9923
efs0-cod3 <eflearnng@gmail.com>
2022-09-23 11:39:30 -0400

    kod

---
+++ 8kyus/volumeOfCuboid.js
@@ -0,0 +1,7 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      // your code here
+      // volume of cuboid formula = V=l*w*h
+      return length*width*height
+    }
+  }
