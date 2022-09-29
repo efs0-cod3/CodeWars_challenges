d8264c23106cfa144f8c9344720d3d57e3f32d88
efs0-cod3 <eflearnng@gmail.com>
2022-09-29 07:31:35 -0400

    kod

--- countingDuplicates.js
+++ countingDuplicates.js
@@ -30,23 +30,27 @@ function duplicateCount(text) {
   ).length;
 }
 
+//  regex
 
-// other method 
+const duplicateCount = (text) =>
+  (text.match(/(\w)(?=(?!.*\1.*\1).*\1)/gi) || []).length;
+
+// other method
 const duplicateCount = (string) => {
-    // makes an array all lowercase and sorts the array in alpha order for easy comparrison
-    let newString = string.toLowerCase().split('').sort();
-    // this array will house the duplicated values so we can 
-    // get the length of it (or the number of duplicated characters). 
-    let newArray = []
-    // set a loop for the array
-    for(i = 0; i < newString.length; i++){
-       // if the current element equals the following element the push it to the new array AND
-       // ONLY if the new array doesn't already include the current element
-       if(newString[i] === newString[i + 1] && !newArray.includes(newString[i])){
-           // push elements to new array
-           newArray.push(newString[i])
-       }
+  // makes an array all lowercase and sorts the array in alpha order for easy comparrison
+  let newString = string.toLowerCase().split("").sort();
+  // this array will house the duplicated values so we can
+  // get the length of it (or the number of duplicated characters).
+  let newArray = [];
+  // set a loop for the array
+  for (i = 0; i < newString.length; i++) {
+    // if the current element equals the following element the push it to the new array AND
+    // ONLY if the new array doesn't already include the current element
+    if (newString[i] === newString[i + 1] && !newArray.includes(newString[i])) {
+      // push elements to new array
+      newArray.push(newString[i]);
     }
-    // return the number of elements in the array to represent the number characters that were duplicated
-    return newArray.length
-}
+  }
+  // return the number of elements in the array to represent the number characters that were duplicated
+  return newArray.length;
+};
