a2dbcc35d488425d7f65759d7578d5d72189d790
efs0-cod3 <eflearnng@gmail.com>
2022-10-03 20:40:32 -0400

    kod

---
+++ oppositesAttract.js
@@ -0,0 +1,8 @@
+function lovefunc(flower1, flower2) {
+  // moment of truth
+  return flower1 % 2 != 0 && flower2 % 2 == 0
+    ? true
+    : flower1 % 2 == 0 && flower2 % 2 != 0
+    ? true
+    : false;
+}
