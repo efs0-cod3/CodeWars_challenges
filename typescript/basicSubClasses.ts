d13fda6f4e641005f248225da7f4126988c8ec65
efs0-cod3 <eflearnng@gmail.com>
2022-10-25 11:38:52 -0400

    tskod

---
+++ typescript/basicSubClasses.ts
@@ -0,0 +1,14 @@
+export class Human{}
+export class Man extends Human{}
+export class Woman extends Human{}
+
+export class God{
+/**
+ * @returns Human[]
+ */
+  static create(){
+    // code
+   return [new Man,new Woman]
+  }
+}
+// code
