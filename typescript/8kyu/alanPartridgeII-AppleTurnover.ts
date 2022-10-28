97044480df5f5f9df3c45d7d22ffd6d224cd978a
efs0-cod3 <eflearnng@gmail.com>
2022-10-28 15:16:39 -0400

    tskod appleTurnover

---
+++ typescript/8kyu/alanPartridgeII-AppleTurnover.ts
@@ -0,0 +1,15 @@
+export function apple(x: number | string): string {
+    return Math.pow(Number(x),2) >= 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' ;
+  }
+
+//   second solution 
+  export function apple2(x: number | string): string {
+    let val: number;
+    if(typeof x === 'string'){
+        val = parseInt(x)
+    } else {
+        val = x
+    }
+
+    return val ** 2 >= 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' ;
+  }
