5f429019207cd88c838c774e5ad27d68bb2ddc72
efs0-cod3 <eflearnng@gmail.com>
2022-11-06 12:42:50 -0400

    tskod

---
+++ typescript/7kyu/sortArrays-1.ts
@@ -0,0 +1,6 @@
+export function sortMe (names: string[]): string[] {
+    // Your code here
+  //   use sort method to "sort" the strings in ascending order
+    return names.sort()
+  }
+  sortMe(["one", "two", "three"])
