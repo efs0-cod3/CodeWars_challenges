d612f2f0523d3fb3f047fbdf4d469c56f5bd5476
efs0-cod3 <eflearnng@gmail.com>
2022-12-03 21:36:11 -0400

    mod

--- 8kyus/bartenderDrinks.js
+++ 8kyus/bartenderDrinks.js
@@ -1,27 +1,42 @@
-function getDrinkByProfession(param){
-    let answer = ''
-    switch(param.toLowerCase()){
-        case "jabroni":
-          answer = "Patron Tequila";
-        break
-        case "school counselor":
-        answer = "Anything with Alcohol"
-        break
-        case "programmer":
-          answer = "Hipster Craft Beer";
-        break
-        case "bike gang member":
-        answer = "Moonshine";
-        break
-        case "politician":
-          answer = "Your tax dollars";
-        break
-        case "rapper":
-        answer = "Cristal";
-        break
-        default:
-          answer = "Beer";
-    }
-    return answer
+// function getDrinkByProfession(param){
+//     let answer = ''
+//     switch(param.toLowerCase()){
+//         case "jabroni":
+//           answer = "Patron Tequila";
+//         break
+//         case "school counselor":
+//         answer = "Anything with Alcohol"
+//         break
+//         case "programmer":
+//           answer = "Hipster Craft Beer";
+//         break
+//         case "bike gang member":
+//         answer = "Moonshine";
+//         break
+//         case "politician":
+//           answer = "Your tax dollars";
+//         break
+//         case "rapper":
+//         answer = "Cristal";
+//         break
+//         default:
+//           answer = "Beer";
+//     }
+//     return answer
+//   }
+// console.log(getDrinkByProfession("jabrOni"));
+
+function getDrinkByProfession(profession){
+  
+  const drinks = {
+    "jabroni": "Patron Tequila",
+    "school counselor": "Anything with Alcohol",
+    "programmer": "Hipster Craft Beer",
+    "bike gang member": "Moonshine",
+    "politician": "Your tax dollars",
+    "rapper": "Cristal"
   }
+
+  return drinks[profession.toLowerCase()] || "Beer"
+}
   console.log(getDrinkByProfession("jabrOni"));
