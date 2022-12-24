44414ea9174384feabaaa55ccc0f1c404e71e3b9
efs0-cod3 <eflearnng@gmail.com>
2022-12-23 20:53:44 -0400

    kod

---
+++ 8kyus/toSqrtOrNotSqr.js
@@ -0,0 +1,9 @@
+function squareOrSquareRoot(array) {
+  return array.map((el) => {
+    if (Math.round(Math.sqrt(el)) ** 2 !== el) {
+      return Math.pow(el, 2);
+    } else {
+      return Math.sqrt(el);
+    }
+  });
+}
