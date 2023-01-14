f74bd23186446e70fcf8b819af50d02d9468918c
efs0-cod3 <eflearnng@gmail.com>
2023-01-14 15:36:37 -0400

    kod

--- 7kyus/alphabetWar.js
+++ 7kyus/alphabetWar.js
@@ -22,3 +22,12 @@ console.log(alphabetWar("z"))
 console.log(alphabetWar("zdqmwpbs"))
 console.log(alphabetWar("zzzzs"))
 console.log(alphabetWar("wwwwww"))
+
+
+//  other solution 
+
+function alphabetWar(fight) {
+    let map = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 };
+    let result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0);
+    return result ? (result < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!";
+}
