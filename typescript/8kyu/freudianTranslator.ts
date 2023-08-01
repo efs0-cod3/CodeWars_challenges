35f63a2ceceae4826c4038606e21ce9dd7e12ed6
efs0-cod3 <eflearnng@gmail.com>
2023-08-01 13:48:28 -0400

    kod ts

--- typescript/8kyu/freudianTranslator.ts
+++ typescript/8kyu/freudianTranslator.ts
@@ -1,8 +1,10 @@
-function toFreud(string) {
-  let result = [];
+export function toFreud(string: string): string {
+  let result: string[] = [];
   if (!string) {
     return "";
   }
   string.split(" ").forEach((el) => result.push("sex"));
   return result.join(" ");
 }
+
+toFreud('oye ahira');
