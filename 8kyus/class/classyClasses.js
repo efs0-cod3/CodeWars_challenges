fcb9688d3c39005bface9fe9c4175397749a3001
efs0-cod3 <eflearnng@gmail.com>
2023-08-16 05:56:43 -0400

    kod

---
+++ 8kyus/class/classyClasses.js
@@ -0,0 +1,18 @@
+// Your task is to complete this Class, 
+// the Person class has been created. 
+// You must fill in the Constructor method 
+// to accept a name as string and an age as number,
+// complete the get Info property and getInfo 
+// method/Info getter which should return johns age is 34
+
+
+class Person {
+    constructor(name, age) {
+     this.name = name;
+     this.age = age;
+    }
+    
+    get info() {
+      return `${this.name}s age is ${this.age}`
+    }
+  }
