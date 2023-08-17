751abf5343d8ac3e9202cd60cc40a59ec9d53752
efs0-cod3 <eflearnng@gmail.com>
2023-08-17 06:04:49 -0400

    kod

---
+++ 8kyus/grassHopper-bugSquashing.js
@@ -0,0 +1,19 @@
+let health = 100
+let position = 0
+let coins = 0
+
+function rollDice(){};
+function move(){};
+function combat(){};
+function getCoins(){};
+function buyHealth(){};
+function printStatus(){};
+
+function main () {
+  rollDice()
+  move()
+  combat()
+  getCoins()
+  buyHealth()
+  printStatus()
+}
