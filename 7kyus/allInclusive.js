fa8fdf15fbb38f38b041ff82aa628133d1081153
efs0-cod3 <eflearnng@gmail.com>
2023-11-22 19:14:52 -0400

    added some inprovements

--- 7kyus/allInclusive.js
+++ 7kyus/allInclusive.js
@@ -3,15 +3,15 @@ function containAllRots(strng, arr) {
   let strToArr = [...strng];
   const allRotations = [];
 
-//   const rots = [...strng].map((_, i) => strng.slice(i) + strng.slice(0, i));
- 
-for (let i = 0; i < strToArr.length; i++) {
-      let last = strToArr.pop();
-      strToArr.unshift(last);
-      allRotations.push(strToArr.join(""));
-    }
-    // console.log({rots,allRotations});
-    
+  //   const rots = [...strng].map((_, i) => strng.slice(i) + strng.slice(0, i));
+
+  for (let i = 0; i < strToArr.length; i++) {
+    let last = strToArr.pop();
+    strToArr.unshift(last);
+    allRotations.push(strToArr.join(""));
+  }
+  // console.log({rots,allRotations});
+
   const verifing = allRotations
     .map((el) => {
       return arr.includes(el);
@@ -20,11 +20,33 @@ for (let i = 0; i < strToArr.length; i++) {
 
   return verifing.length == allRotations.length;
 }
-console.log(containAllRots("bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]));
-
+console.log(
+  containAllRots("bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"])
+);
 
 // shorter approach
 function containAllRotsS(strng, arr) {
-    return [...strng].map((_,i) => strng.substr(i)+strng.slice(0,i)).every(x => arr.includes(x));
+  return [...strng]
+    .map((_, i) => strng.substr(i) + strng.slice(0, i))
+    .every((x) => arr.includes(x));
 }
-console.log(containAllRotsS("bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]));
+console.log(
+  containAllRotsS("bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"])
+);
+
+function containAllRotsB(strng, arr) {
+  // your code
+  let strToArr = [...strng];
+  const allRotations = [];
+
+  for (let i = 0; i < strToArr.length; i++) {
+    let last = strToArr.pop();
+    strToArr.unshift(last);
+    allRotations.push(strToArr.join(""));
+  }
+
+  return allRotations.map((el) => el).every((el) => arr.includes(el));
+}
+console.log(
+  containAllRotsB("bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"])
+);
