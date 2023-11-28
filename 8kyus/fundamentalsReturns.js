04e65cdf51cfd539e36893068accc605c47a77ea
efs0-cod3 <eflearnng@gmail.com>
2023-11-28 19:27:59 -0400

    returns 8k

---
+++ 8kyus/fundamentalsReturns.js
@@ -0,0 +1,30 @@
+function add(a,b){
+    return a+b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function mod(a,b){
+    return a%b
+}
+   
+function exponent(a,b){
+    return a**b
+}
+    
+function subt(a,b){
+    return a-b
+}
+
+console.log(add(1,2))
+console.log(multiply(1,2))
+console.log(divide(2,1))
+console.log(mod(1,2))
+console.log(exponent(1,2))
+console.log(subt(1,2),)
