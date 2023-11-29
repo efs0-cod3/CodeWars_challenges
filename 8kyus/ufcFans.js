26ecd9f04f1d328020929b6765cf398227d2aca4
efs0-cod3 <eflearnng@gmail.com>
2023-11-29 06:58:43 -0400

    ufc 8k

---
+++ 8kyus/ufcFans.js
@@ -0,0 +1,11 @@
+var quote = function(fighter) {
+    // your code here
+    return fighter.toLowerCase() == 'george saint pierre' 
+      ? "I am not impressed by your performance." 
+      : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  };
+
+  console.log(quote('GEORGE SAINT PIERRE'));
+  console.log(quote('CONOR MCGREGOR'));
+
+    
