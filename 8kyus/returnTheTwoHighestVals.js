a257b574edb311454fb264c3d3034659b946f1fa
efs0-cod3 <eflearnng@gmail.com>
2023-12-13 08:32:18 -0400

    kod

---
+++ 8kyus/returnTheTwoHighestVals.js
@@ -0,0 +1,25 @@
+function twoHighest(arr) {
+    let result = []
+    let checker = 0
+    
+    if(arr.length == 0 || arr.length == 1){
+      return arr
+    }
+    
+    arr = arr.sort((min,max) => min - max)
+    result.push(arr[arr.length - 1])
+    
+    for(let i =0; i < arr.length; i++){
+      if(arr[i] > checker && !result.includes(arr[i])){
+        checker = arr[i]
+      }
+    }
+    result.push(checker)
+      return result
+    
+  }
+
+  console.log(twoHighest([15, 20, 20, 17]));
+  console.log(twoHighest([15]));
+  console.log(twoHighest([]));
+  
