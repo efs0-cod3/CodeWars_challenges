79f65f725f2b586373e328ea13b65f629e5e5d57
efs0-cod3 <eflearnng@gmail.com>
2024-02-12 22:08:02 -0400

    kod

---
+++ 8kyus/leodicaprioAndOscars.js
@@ -0,0 +1,9 @@
+function leo(oscar){
+    return oscar > 88 ? "Leo got one already!" 
+     : oscar == 88 
+      ? "Leo finally won the oscar! Leo is happy"
+     : oscar == 86
+      ? "Not even for Wolf of wallstreet?!"
+     : "When will you give Leo an Oscar?"
+     
+   }
